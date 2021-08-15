70e65ee9933d236c3cac6e03bf7672c0178da452
Travis Phasoukvong <travisphasoukvong@hotmail.com>
2021-08-15 00:55:34 -0400

    Add functions

---
+++ script.js
@@ -0,0 +1,45 @@
+const choices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
+function computerPlay() 
+{
+    let choice = Math.floor(Math.random()*choices.length);
+    return choices[choice];
+}
+    
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie game!";
+    } else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")
+        ) {
+            playerScore++;
+            return `You Win! ${playerSelection} beats ${computerSelection}.`;
+    } else {
+        computerScore++;
+        return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+}
+
+function game() {
+    while (playerScore != 5 && computerScore !=5) {
+        let userChoice = prompt("Rock, paper, or scissors?").toLowerCase();
+
+        while (choices.includes(userChoice) == false) {
+            userChoice = prompt("Choice not valid. Rock, paper, or scissors?").toLowerCase();
+        }
+
+        console.log(playRound(userChoice, computerPlay()));
+        console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`);
+    }
+    if (playerScore == 5) {
+        return "Congratulations! You win!";
+    } else {
+        return "Sorry, better luck next time.";
+    }
+}
+
+game();
