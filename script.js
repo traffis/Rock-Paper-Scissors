1114dc3d34f5d79656a49c07dd6b4471895dce62
Travis Phasoukvong <travisphasoukvong@hotmail.com>
2021-10-21 02:42:26 -0400

    Refactor functions to work with HTML elements

--- script.js
+++ script.js
@@ -7,34 +7,53 @@ function computerPlay()
     let choice = Math.floor(Math.random()*choices.length);
     return choices[choice];
 }
+
+function updateScore(playerSelection, computerSelection) {
+    const playerScoreId = document.querySelector("#player-score");
+    const computerScoreId = document.querySelector("#computer-score");
+
+    playerScoreId.textContent = playerScore;
+    computerScoreId.textContent = computerScore;
+    
+}
     
 function playRound(playerSelection, computerSelection) {
+    const results = document.querySelector("#results");
     if (playerSelection == computerSelection) {
-        return "Tie game!";
+        results.textContent = "Tie game!";
     } else if (
         (playerSelection == "rock" && computerSelection == "scissors") ||
         (playerSelection == "paper" && computerSelection == "rock") ||
         (playerSelection == "scissors" && computerSelection == "paper")
         ) {
             playerScore++;
-            return `You Win! ${playerSelection} beats ${computerSelection}.`;
+            results.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
     } else {
         computerScore++;
-        return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+        results.textContent = `You lost! ${playerSelection} loses to ${computerSelection}.`;
     }
+    updateScore(playerSelection, computerSelection);
 }
 
 function game() {
-    while (playerScore != 5 && computerScore !=5) {
-        let userChoice = prompt("Rock, paper, or scissors?").toLowerCase();
+    const buttons = document.querySelectorAll('button');
+    const results = document.querySelector("#results");
 
-        while (choices.includes(userChoice) == false) {
-            userChoice = prompt("Choice not valid. Rock, paper, or scissors?").toLowerCase();
-        }
-
-        console.log(playRound(userChoice, computerPlay()));
-        console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`);
-    }
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            console.log(playRound(button.id, computerPlay()));
+            
+            if (playerScore == 5) {
+                playerScore = computerScore = 0;
+                alert("Congrats, you win!");
+                results.textContent = "Select your hand."
+            } else if (computerScore == 5) {
+                playerScore = computerScore = 0;
+                alert("Sorry, you lost!");
+                results.textContent = "Select your hand."
+            }
+        });
+    });
 }
 
 game();
